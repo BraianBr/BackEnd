1431c5e2e93dd3e197ba032c53a5cc90f82457e8
Braian Ezequiel Rodriguez <Braian2.0roket@gmail.com>
2023-05-16 19:52:33 -0300

    Docker exitoso

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ibm-semeru-runtimes:open-11-jre-focal
+FROM ibm-semeru-runtimes:open-17-jre
 EXPOSE 8080
 ADD ./target/*.jar ./app.jar
 CMD java -Djava.security.egd=file:/dev/./urandon -jar app.jar
--- src/main/java/com/projecto/integrador/br/Security/Controller/AuthController.java
+++ src/main/java/com/projecto/integrador/br/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontend-br.web.app/"})
+@CrossOrigin(origins = {"https://frontend-br.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
