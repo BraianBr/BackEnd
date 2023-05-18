6c60613bc909fca3461cb58806733a614b5f0e2f
Braian Ezequiel Rodriguez <Braian2.0roket@gmail.com>
2023-05-18 14:20:15 -0300

    Funcionalidades al 100%

--- src/main/java/com/projecto/integrador/br/Security/Controller/AuthController.java
+++ src/main/java/com/projecto/integrador/br/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontend-br.web.app"})
+@CrossOrigin(origins = {"https://frontend-br.web.app","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
